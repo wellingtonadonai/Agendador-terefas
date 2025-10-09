41b1814a319d107ea31009e07f04f8e3ee62c613
wellington <wellington.h.s.oliveira@hotmail.com>
2025-10-09 19:09:48 -0300

    commit inicial

---
+++ src/test/java/br/com/wellington/agendador_tarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wellington.agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
