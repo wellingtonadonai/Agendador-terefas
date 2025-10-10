9792fe7c83d73d594a43778ae4f41beb89ee3b89
wellington <wellington.h.s.oliveira@hotmail.com>
2025-10-10 10:21:23 -0300

    correcao do codigo

--- src/main/java/br/com/wellington/agendador_tarefas/infraestructure/client/UsuarioClient.java
+++ src/main/java/br/com/wellington/agendador_tarefas/infraestructure/client/UsuarioClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "usuario", url = "${usuario.url}")
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDto buscaUsuarioPorEmail(@RequestParam("email") String email,
                                     @RequestHeader("Authorization")String token);
 }
